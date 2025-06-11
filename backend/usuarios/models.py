40af77c849250936c13dc18a3d2d92b36a4d0c42
MarcosViniciusG <marcos.vinicius.gundel.silva@gmail.com>
2025-06-10 22:03:42 -0300

    feature: adicionado as classes Usuario, UsuarioSerializer Co-authored-by: Pedro Teixeira <pedrotms21@icloud.com>

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome', 'username']
+
+    def __str__(self):
+        return self.email
