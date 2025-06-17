ade7bbfc3c25afcea690bddf9b75ffb71e392789
Withy <davilf2004@gmail.com>
2025-06-16 21:50:13 -0300

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    funções = [('agricultor', 'Agricultor'), ('tecnico', 'Técnico'), ('administrador', 'Administrador'), ('analista', 'Analista de crédito')]
-    role = models.CharField(max_length=20, choices=funções)
+    role_functions = [('agricultor', 'Agricultor'), ('tecnico', 'Técnico'), ('administrador', 'Administrador'), ('analista', 'Analista de crédito')]
+    role = models.CharField(max_length=20, choices=role_functions)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name', 'username']
 
