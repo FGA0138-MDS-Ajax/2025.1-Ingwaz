d730df974776919427788952006a312dec6c3d6a
Withy <davilf2004@gmail.com>
2025-06-16 22:50:02 -0300

    Add files via upload

---
+++ backend/propriedade/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   
+    path('propriedade/', views.PropriedadeListCreateView.as_view(), name='propriedade-list-create'),
+    
+  
+    path('propriedade/<int:pk>/', views.PropriedadeDetailView.as_view(), name='propriedade-detail'),
+]
