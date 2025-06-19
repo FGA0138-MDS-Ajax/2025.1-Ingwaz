04fe0365b671b5dbbc7982f52e61e56427d671ec
yogi <yogiqhz@gmail.com>
2025-06-19 19:22:01 -0300

    fix: mudança nas rotas pra compatibilidade com redocs (deixar todas as rotas de plantios juntas)

--- backend/plantios/views.py
+++ backend/plantios/views.py
@@ -1,21 +1,12 @@
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Plantio
 from .serializers import PlantioSerializer
 
-class RegisterPlantio(generics.CreateAPIView):
-    queryset = Plantio.objects.all()
+class PlantioListCreateView(generics.ListCreateAPIView):
     serializer_class = PlantioSerializer
-
-class ListPlantios(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
-        user = request.user
-        plantios = Plantio.objects.filter(propriedade__agricultor = user)
-        serializer = PlantioSerializer(plantios, many= True)
-        return Response(serializer.data)     
-        
+    def get_queryset(self):
+        user = self.request.user
+        return Plantio.objects.filter(propriedade__agricultor=user)
