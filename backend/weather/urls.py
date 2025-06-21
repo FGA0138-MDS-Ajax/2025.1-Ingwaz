db3c325df9f86838f4b299e963bf4e9173b480ef
MarcosViniciusG <marcos.vinicius.gundel.silva@gmail.com>
2025-06-21 16:04:59 -0300

    fix: adicionado '/' no final da rota weather/<int:propriedade_id>/<bool:cache>/

--- backend/weather/urls.py
+++ backend/weather/urls.py
@@ -6,7 +6,7 @@ register_converter(BoolConverter, 'bool')
 
 urlpatterns = [
     # Endpoint para a lista de previsões da semana de uma propriedade
-    path('weather/<int:propriedade_id>/<bool:cache>', WeatherListView.as_view(), name='weather-list-for-property'),
+    path('weather/<int:propriedade_id>/<bool:cache>/', WeatherListView.as_view(), name='weather-list-for-property'),
     
     # Endpoint para a previsão de um dia específico de uma propriedade
     path('weather/<int:propriedade_id>/<str:data>/', WeatherDetailView.as_view(), name='weather-detail-for-property'),
