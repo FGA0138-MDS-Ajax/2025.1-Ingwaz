22ff8f45e5143389fd8c0d581da99e5160e2de03
daviegito <daviegitocoelho@gmail.com>
2025-06-26 14:55:48 -0300

    test: casos de teste acrescentados para o credito. Em breve, mais testes para credito e depois para a API

--- backend/tests/unit/credito/test_credito.py
+++ backend/tests/unit/credito/test_credito.py
@@ -3,45 +3,190 @@ from credito.serializers import SolicitacaoCreditoSerializer
 from credito.serializers import SolicitacaoCreditoCreateSerializer
 from credito.models import SolicitacaoCredito
 from rest_framework import serializers
+from users.models import User 
+from propriedade.models import Propriedade
+from plantios.models import Plantio
+from datetime import date, timedelta, datetime
 
-# @pytest.mark.django_db
-# def test_criacao_credito():
+@pytest.mark.django_db
+def test_criacao_credito():
 #verificar se a criação de crédito é possível com atributos corretos
-  # solicitacao1 = SolicitacaoCredito.objects.create(
-  #   user = '',
-  #   plantio = '',
-  #   propriedade = '',
-  #   score = '',
-  #   status = '',
-  #   created_at = '',
-  #   updated_at = ''
-  # )   
-  # assert solicitacao1.user == ''
-  # assert solicitacao1.plantio == ''
-  # assert solicitacao1.propriedade == ''
-  # assert solicitacao1.score == ''
-  # assert solicitacao1.status == ''
-  # assert solicitacao1.created_at == ''
-  # assert solicitacao1.updated_at == ''
+  usuario1 = User.objects.create_user(
+        username='joao123',
+        name='joao',
+        email='joao@uol.com.br',
+        password='senha123',
+        role='agricultor',
+        cpf='123.456.789-09'
+    )
 
-# @pytest.mark.django_db
-# def test_criacao_campos_vazios():
-#verificar se a criação de crédito é possível com atributos corretos
-  # solicitacao1 = SolicitacaoCredito.objects.create(
-  #   user = '',
-  #   plantio = '',
-  #   propriedade = '',
-  #   score = '',
-  #   status = '',
-  #   created_at = '',
-  #   updated_at = ''
-  # )   
-  # serializer = SolicitacaoCreditoCreateSerializer
-  # assert solicitacao1.user == ''
-  # assert solicitacao1.plantio == ''
-  # assert solicitacao1.propriedade == ''
-  # assert solicitacao1.score == ''
-  # assert solicitacao1.status == ''
-  # assert solicitacao1.created_at == ''
-  # assert solicitacao1.updated_at == ''
+  propriedade1 = Propriedade.objects.create(
+    nome = 'chácara feliz',
+    area_total = 450,
+    latitude=-15.6,
+    longitude=-46.6,
+    agricultor = usuario1
+  )
 
+  plantio1 = Plantio.objects.create(
+    cultura = 'Soja',
+    area = 30,
+    data = str(date.today()),
+    estimativa_colheita = str(date.today()),
+    propriedade = propriedade1
+  ) 
+    
+  solicitacao1 = SolicitacaoCredito.objects.create(
+    user = usuario1,
+    plantio = plantio1,
+    propriedade = propriedade1,
+    score = 1.0,
+    status = 'aprovado',
+    created_at = str(date.today()),
+    updated_at = str(date.today())
+  )  
+
+  assert solicitacao1.score == 1.0
+  assert solicitacao1.status == 'aprovado'
+  assert solicitacao1.created_at.strftime("%Y-%m-%d %H:%M:%S") == str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+  assert solicitacao1.updated_at.strftime("%Y-%m-%d %H:%M:%S") == str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+@pytest.mark.django_db
+def test_criacao_sem_score():
+#verificar se ele aceita a criação de uma solicitação sem um campo optativo
+  usuario1 = User.objects.create_user(
+      username='joao123', 
+      name='Joao', 
+      email='joao@uol.com.br',
+      password='senha123', 
+      role='agricultor', 
+      cpf='123.456.789-09'
+  )
+  propriedade1 = Propriedade.objects.create(
+      nome='Chácara feliz', 
+      area_total=450, 
+      latitude=-15.6, 
+      longitude=-46.6, 
+      agricultor=usuario1
+  )
+  plantio1 = Plantio.objects.create(
+      cultura='Soja', 
+      area=30, 
+      data=date.today(), 
+      estimativa_colheita=date.today(), 
+      propriedade=propriedade1
+  )
+  solicitacao1 = SolicitacaoCredito.objects.create(
+      user=usuario1, 
+      plantio=plantio1, 
+      propriedade=propriedade1, 
+      status='aprovado'
+  )
+  assert solicitacao1.score is None or solicitacao1.score == 0
+
+@pytest.mark.django_db
+def test_credito_status_invalido():
+  usuario1 = User.objects.create_user(
+      username = 'joao123', 
+      name = 'Joao', 
+      email = 'joao@uol.com.br',
+      password = 'senha123', 
+      role = 'agricultor', 
+      cpf = '123.456.789-09'
+  )
+  propriedade1 = Propriedade.objects.create(
+      nome = 'Chácara feliz', 
+      area_total = 450, 
+      latitude = -15.6, 
+      longitude = -46.6, 
+      agricultor = usuario1
+  )
+  plantio1 = Plantio.objects.create(
+      cultura = 'Soja', 
+      area = 30, 
+      data = date.today(), 
+      estimativa_colheita = date.today(), 
+      propriedade = propriedade1
+  )
+  with pytest.raises(Exception):
+    dados = {
+      'user': usuario1,
+      'plantio': plantio1,
+      'propriedade': propriedade1,
+      'score': 1.0,
+      'status': 'nenhumstatus'
+    }
+    serializer = SolicitacaoCreditoSerializer(data=dados)
+    assert not serializer.is_valid()
+    assert 'status' in serializer.errors
+
+@pytest.mark.django_db
+def test_str_solicitacao():
+#verificar se a string retorna a mesma que está em credito.models
+  user = User.objects.create_user(
+      username = 'joao123', 
+      name = 'Joao', 
+      email = 'joao@uol.com.br',
+      password = 'senha123', 
+      role = 'agricultor', 
+      cpf = '123.456.789-09'
+  )
+  propriedade1 = Propriedade.objects.create(
+      nome = 'Chácara feliz', 
+      area_total = 450, 
+      latitude = -15.6, 
+      longitude = -46.6, 
+      agricultor = user
+  )
+  plantio1 = Plantio.objects.create(
+      cultura = 'Soja', 
+      area = 30, 
+      data = date.today(), 
+      estimativa_colheita = date.today(), 
+      propriedade = propriedade1
+  )
+  solicitacao1 = SolicitacaoCredito.objects.create(
+    user = user,
+    plantio = plantio1,
+    propriedade = propriedade1,
+    score = 1.0,
+    status = 'pendente'
+  )
+  assert str(solicitacao1) == f"Solicitação {solicitacao1.id} - Status: {solicitacao1.status} - User: {solicitacao1.user.email}"
+
+@pytest.mark.django_db
+def test_atualizacao_score():
+#verificar se a string retorna a mesma que está em credito.models
+  user = User.objects.create_user(
+      username = 'joao123', 
+      name = 'Joao', 
+      email = 'joao@uol.com.br',
+      password = 'senha123', 
+      role = 'agricultor', 
+      cpf = '123.456.789-09'
+  )
+  propriedade1 = Propriedade.objects.create(
+      nome = 'Chácara feliz', 
+      area_total = 450, 
+      latitude = -15.6, 
+      longitude = -46.6, 
+      agricultor = user
+  )
+  plantio1 = Plantio.objects.create(
+      cultura = 'Soja', 
+      area = 30, 
+      data = date.today(), 
+      estimativa_colheita = date.today(), 
+      propriedade = propriedade1
+  )
+  solicitacao1 = SolicitacaoCredito.objects.create(
+    user = user,
+    plantio = plantio1,
+    propriedade = propriedade1,
+    score = 0.9,
+    status = 'aprovado'
+  )
+
+  solicitacao1.score = 1.0
+  solicitacao1.save()
+  assert solicitacao1.score == 1.0
