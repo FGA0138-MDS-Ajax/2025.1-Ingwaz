4054ec62b1a67e7732eab990346cfdefc497d33d
MarcosViniciusG <marcos.vnc.gundel.silva@gmail.com>
2025-06-30 23:22:32 -0300

    fix: resolve bug do merge onde getQuotes e getPerguntas são declarados 2x

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -76,55 +76,6 @@ export async function getPerguntas(pergunta) {
   }
 }
 
-export async function getQuotes() {
-  try {
-    console.log(`${API_BASE}/quotes/`);
-    const response = await fetch(`${API_BASE}/quotes/`);
-    const data = await response.json();
-    if (!response.ok) {
-      console.error("Erro API:", data);
-      return null;
-    }
-    return data;
-  } catch (e) {
-    console.error("Erro servidor:", e.message);
-    return null;
-  }
-}
-
-export async function getPerguntas(pergunta) {
-  try {
-    payload = {
-      "id": "query_all",
-      "params": {
-        "query_string": pergunta,
-        "from": 0,
-        "size": 100
-      }
-    };
-    const response = await fetch(
-      API_EMBRAPA_URL, {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${API_EMBRAPA_KEY}`,
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(payload),
-      },
-    );
-    
-    const data = await response.json();
-    if (!response.ok) {
-      console.error("Erro API:", data);
-      return null;
-    }
-    return data.hits.hits;
-  } catch (e) {
-    console.error("Erro servidor:", e.message);
-    return null;
-  }
-}
-
 const getAuthToken = async () => {
   return await AsyncStorage.getItem('token');
 };
