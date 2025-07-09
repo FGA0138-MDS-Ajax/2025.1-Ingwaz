82846882e1494f36bb36eebaf1ba53e7116c5797
Milena Marques <84979688+milenamso@users.noreply.github.com>
2025-07-09 00:46:54 -0300

    Update test_api_quotes.py

--- backend/tests/api/quotes/test_api_quotes.py
+++ backend/tests/api/quotes/test_api_quotes.py
@@ -1,23 +1,28 @@
 from rest_framework.test import APITestCase
+from users.serializers import UserSerializer
+from users.models import User
+from unittest.mock import patch
 from django.urls import reverse
 from quotes.models import Quote
 from django.contrib.auth import get_user_model
-from unittest.mock import patch
 
 
 class QuoteListViewTest(APITestCase):
     def test_lista_vazia_cotacoes(self):
+        # Verificar a resposta quando não tem cotações no banco
         url = reverse('quote-list')
         response = self.client.get(url)
         assert response.status_code == 200
         assert response.data == []
 
     def test_acesso_nao_autenticado(self):
+        # Verificar se ele aceita acesso sem autenticação à lista de cotações
         url = reverse('quote-list')
         response = self.client.get(url)
         assert response.status_code == 200
 
     def test_filtro_nome_API(self):
+        # Verificar se dá pra filtrar o nome pra buscar uma cultura específica
         Quote.objects.create(date='2024-10-17', name='soja', unity='kg', value=70)
         Quote.objects.create(date='2024-10-17', name='milho', unity='kg', value=80)
         url = reverse('quote-list')
@@ -26,14 +31,16 @@ class QuoteListViewTest(APITestCase):
         assert all(q['name'].lower() == 'soja' for q in response.data)
 
     def test_atualizacao_lote_sem_autenticacao(self):
+        # Verificar se o lote pode ser atualizado sem autenticação
         url = reverse('quote-update')
         response = self.client.post(url)
         assert response.status_code == 401
 
+    # Esses mocks foram colocados pra diminuir o tempo de execução deste teste no pytest (estava em 33s)
     @patch('quotes.views.get_all_cepea_quotes', return_value=([], True))
     @patch('quotes.views.get_all_hfbrasil_quotes', return_value=([], True))
     def test_atualizacao_em_lote_com_autenticacao(self, mock_get_all_hfbrasil_quotes, mock_get_all_cepea_quotes):
-        # Cria superusuário com permissões administrativas
+        # Criar um usuário que irá fazer o login com permissão de admin
         User = get_user_model()
         user = User.objects.create_superuser(
             username='admin',
@@ -41,21 +48,17 @@ class QuoteListViewTest(APITestCase):
             password='senha123'
         )
 
-        # Realiza login via API
+        # Realizar o login via API
         url_login = reverse('login')
-        response = self.client.post(url_login, {
-            'email': 'admin@admin.com',
-            'password': 'senha123'
-        })
-
+        response = self.client.post(url_login, {'email': 'admin@admin.com', 'password': 'senha123'})
         assert response.status_code == 200
         assert 'token' in response.data
         token = response.data['token']
 
-        # Adiciona token no header de autenticação
+        # Agora vamos usar o token pra validar a autenticação
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
 
-        # Realiza requisição à view protegida
+        # Chamada autenticada para atualizar as cotações
         url = reverse('quote-update')
         response = self.client.post(url)
         assert response.status_code == 200
